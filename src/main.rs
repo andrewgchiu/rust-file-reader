9a61216486850ee0c603aa999d235df008aaf54b
Andrew Chiu <andrew.g.chiu@gmail.com>
2024-03-20 16:03:34 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+
+    // The second argument is the user provided argument.
+    let user_path: String = args[1].clone();
+    println!("My user provided argument is {}.", user_path);
+
+    let file = File::open(user_path);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
